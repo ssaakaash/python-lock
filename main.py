127a88b1f2770b11ea594a5ba095021626a1b0a7
Anurag <anuragrath1902@gmail.com>
2022-05-18 16:18:02 +0530

    added print

--- main.py
+++ main.py
@@ -12,5 +12,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print("Hello")
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
