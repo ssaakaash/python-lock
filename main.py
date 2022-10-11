07a8e207e5e8fee1b9a5bc5d452df1ade53b7c1d
aakaash <ssaakaash@icloud.com>
2022-10-11 20:08:58 +0530

    Added Layouts

--- main.py
+++ main.py
@@ -13,27 +13,40 @@ class Election:
 
         mainframe = ttk.Frame(root, padding='3 3 12 12')
         mainframe.grid(column=0, row=0, sticky="N W E S")
+
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
 
         self.logo = PhotoImage(file=LOGO_PATH)
-        self.logo1 = self.logo.subsample(4, 4)
+        self.logo1 = self.logo.subsample(3, 3)
 
         ttk.Label(mainframe, image=self.logo1).grid(row=0, column=2)
 
-        ttk.Label(mainframe, text='ELECTIONS').grid(row=2, column=2)
+        heading = ttk.Label(mainframe, text='ELECTIONS')
+        heading['font'] = "Alta_light", 150
+        heading.grid(row=2, column=2)
         yr = date.today().year
 
-        ttk.Label(mainframe, text=f'{yr}-{yr - 1999}').grid(row=3, column=2)
+        year = ttk.Label(mainframe, text=f'{yr}-{yr - 1999}')
+        year['font'] = "Alta_light", 80
+        year.grid(row=3, column=2, sticky='N')
 
-        ttk.Button(mainframe, text='Setup').grid(row=4, column=1)
-        ttk.Button(mainframe, text='Start').grid(row=4, column=3)
+        Button(mainframe, text='Set up', font='TkHeadingFont 30', bg='#323232')\
+            .grid(row=4, column=1, sticky='N S E W')
+        Button(mainframe, text='Start', font='TkHeadingFont 30', bg='#323232')\
+            .grid(row=4, column=3, sticky='N S E W')
 
         for child in mainframe.winfo_children():
             child.grid_configure(padx=5, pady=5)
 
+        for n in range(mainframe.grid_size()[1]):
+            mainframe.rowconfigure(n, weight=1)
+            mainframe.columnconfigure(n, weight=1)
+
 
 if __name__ == '__main__':
     root = Tk()
+    #root.attributes('-fullscreen', True)
+    root.state('zoomed')
     app = Election(root)
     root.mainloop()
