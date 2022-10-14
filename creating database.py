82d861cf4847f75c307bc651783e6953278aa8f4
Aakaash SS <ssaakaash@icloud.com>
2022-10-14 09:25:28 +0530

    Delete creating database.py

--- creating database.py
+++
@@ -1,7 +0,0 @@
-# creating database
-import mysql.connector as ms
-con=ms.connect(host='localhost',user='root',passwd='       ')
-cur=con.cursor()
-cur.execute('create database if not exists Password_manager')
-cur.execute('use Password_manager')
-cur.execute('create table if not exists Details (ID int(5),Username varchar(20),Password varchar(15),Category varchar(20)')
